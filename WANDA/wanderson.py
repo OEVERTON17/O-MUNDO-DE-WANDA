8f25de53c0b149604d62c934b5a031e61fc809dd
OEVERTON17 <oevertonsilva3475@gmail.com>
2023-10-28 12:12:43 -0300

    wanderson.py

--- WANDA/wanderson.py
+++ WANDA/wanderson.py
@@ -9,6 +9,15 @@ def animacao_personagem():
     
     janela.blit(jogador_parado_superficies[int(jogador_index)], jogador_retangulo)
 
+def animacao_npc():
+    global npc_index
+    npc_index += 0.11
+    if npc_index > len(npc_parado_superficies) - 1:
+        npc_index = 0
+        
+        janela.blit(npc_parado_superficies[int(npc_index)], npc_retangulo)
+
+    
 # Inicializa o Pygame
 pygame.init()
 
@@ -27,6 +36,15 @@ fundo_imagem = pygame.transform.scale(fundo_imagem, (1530, 800))
 # Carrega as imagens do personagem
 jogador_index = 0
 jogador_parado_superficies = []
+                                     
+# Carrega as imagens do npc 
+
+npc_index = 0 
+npc_parado_superficies = []
+                                     
+                                     
+                                     
+                                     
                                             
 # Carrega o Personagem parado                                            
 for imagem in range(1, 7):
@@ -34,39 +52,22 @@ for imagem in range(1, 7):
     img = pygame.transform.scale(img, (300, 300))
     jogador_parado_superficies.append(img)
 
-# Carrega as imagens do npc
-npc1_index_index = 0
-npc1_superficies = []
-
-
-# carrega o  npc 
-
-for imagem in range(1, 7):
-    img = pygame.image.load(f'WANDA/NPC_1/ANDAR/tile{imagem}.png').convert_alpha()
+# Carrega o NPC parado
+for imagem in range(1, 4):
+    img = pygame.image.load(f'WANDA/NPC/PARADO/PARADO{imagem}.png').convert_alpha()
     img = pygame.transform.scale(img, (300, 300))
-    npc1_superficies.append(img)
-
-
-
-def animacao_npc1():
-    global npc1_index
-
-    npc1_index += 0.11
-    if npc1_index >= len(npc1_superficies):
-       npc1_index = 0
-
-    # Ajusta a posição do npc1 para o lado direito da tela
-    npc1_retangulo.x = tamanho[0] - npc1_retangulo.width
-
-    # Desenha o npc na tela
-    janela.blit(pygame.transform.flip(npc1_superficies[int(npc1_index)], True, False), npc1_retangulo.topleft)
-
-
+    npc_parado_superficies.append(img)
 
 
 # Velocidade do movimento do personagem
 jogador_retangulo = pygame.Rect(50, 50, 50, 50)
 movimento_personagem = 5
+
+
+# Posição inicial do NPC
+npc_retangulo = pygame.Rect( 50, 50, 50, 50)
+
+
 
 
 
@@ -86,6 +87,23 @@ while True:
     keys = pygame.key.get_pressed()
     jogador_retangulo.x += (keys[pygame.K_d] - keys[pygame.K_a]) * movimento_personagem
     jogador_retangulo.y += (keys[pygame.K_s] - keys[pygame.K_w]) * movimento_personagem
+    
+    
+    if jogador_retangulo.y > 400:
+        jogador_retangulo.y = 400;
+    
+    if jogador_retangulo.y < 60:
+        jogador_retangulo.y = 60;
+        
+    if jogador_retangulo.x  > 1320:
+        jogador_retangulo.x  = 1320;
+    
+    if   jogador_retangulo.x < 60:
+        jogador_retangulo.x = 60;  
+       
+
+             
+         
    
 # Desenha a imagem de fundo na posição 
     janela.blit(fundo_imagem,(0, 0))
